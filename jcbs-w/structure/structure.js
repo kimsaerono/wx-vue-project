7f92dd4d7339a85099b13bcdad6f56f65c5e3ffe
kim <1353313426@qq.com>
2018-12-14 17:17:38 +0800

    配置‘npm run structure’命令来copy一份静态资源到本地

---
+++ jcbs-w/structure/structure.js
@@ -0,0 +1,32 @@
+
+const webpack = require('webpack')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const chalk = require('chalk')
+const ora = require('ora')
+const spinner = ora('Copy for static...')
+spinner.start();
+
+const webpackCopyConfig = {
+  entry: {
+    app: './src/main.js'
+  },
+  plugins:[
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static'),
+        to: path.resolve(__dirname , "../staticProd"),
+        ignore: ['.*']
+      }
+    ])
+  ]
+}
+webpack(webpackCopyConfig,(err, stats) => {
+  spinner.stop();
+  console.log(chalk.cyan('  Copy complete.\n'))
+  console.log(chalk.yellow(
+    '  Tip: Copy files have already exist in your project '
+  ))
+})
+
+
