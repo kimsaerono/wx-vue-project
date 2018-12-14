04a4004b4afdd725918243807ea77320af420d26
kim <1353313426@qq.com>
2018-12-14 10:01:39 +0800

    Update main.js
    将vue ,axios添加为外部依赖

--- jcbs-w/src/main.js
+++ jcbs-w/src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // import apiConfig from '../config/api.config'
-import Vue from 'vue'
+
+//import资源
+// import Vue from 'vue'
+// import axios from 'axios';
+import Mint from 'mint-ui';
+//cdn:
+const Vue = require('vue')
+const axios = require('axios')
+// const Mint = require('mint-ui')
+  
 import App from './App.vue'
 import router from './router'
-import axios from 'axios';
 import Mint from 'mint-ui';
 import { MessageBox } from 'mint-ui';
 // axios.defaults.baseURL = apiConfig.baseUrl
